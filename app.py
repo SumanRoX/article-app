5987e48c10503c222da81dc64de1a4ace5cce469
Suman Roy <36808582+SumanRoX@users.noreply.github.com>
2020-08-18 20:54:27 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ Articles = Articles()
 #config mysql
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='CYBERknight007'
+app.config['MYSQL_PASSWORD']=''
 app.config['MYSQL_DB']='myflaskapp'
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 
